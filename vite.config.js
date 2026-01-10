4bc2ac31adf802b37252cb6a75cc2fbcff6b80dd
William Lebris <williamtissi2006@gmail.com>
2026-01-09 23:48:30 -0800

    Working local version ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace 'your-repo-name' with the GitHub repo name you will create
 export default defineConfig({
   plugins: [react()],
-  base: '/Will-s-website/',   // IMPORTANT for GitHub Pages
+  base: '/Will-s-website-new/', 
 })
